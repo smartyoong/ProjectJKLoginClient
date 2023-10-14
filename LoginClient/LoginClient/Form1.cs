2b961f05d3358e60c72b2953e9fe4b0af7382673
Kim Young Chan <smartyoong@naver.com>
2023-10-15 01:33:47 +0900

    데이터 직렬화 함수 및 패킷 생성 완료 다음 번에는 데이터를 입력받아서 실제로 전송하는 방법 추가 예정

--- LoginClient/LoginClient/Form1.cs
+++ LoginClient/LoginClient/Form1.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace LoginClient
 {
@@ -43,6 +46,44 @@ namespace LoginClient
         {
             LoginClientSocket.Shutdown(SocketShutdown.Both);
             LoginClientSocket.Close();
+        }
+    }
+
+    [Serializable]
+    public class LoginMessagePacket
+    {
+        // 변수는 아무렇게나 추가 가능
+        public LOGIN_CLIENT_PACKET_ID IDNum { get; set; }
+        private byte[] Data = null;
+        private int MessageSize = 0;
+        public string StringValue1 { get; set; } = null;
+        public string StringValue2 { get; set; } = null;
+        public int IntegerValue1 { get; set; } = 0;
+
+        public void SerializeData()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            MemoryStream stream = new MemoryStream();
+            formatter.Serialize(stream, this);
+            byte[] bytes = stream.ToArray();
+
+            // 데이터 길이 구하기
+            MessageSize = bytes.Length + sizeof(int);
+
+            // 데이터 길이를 바이트 배열로 변환
+            byte[] LengthBytes = BitConverter.GetBytes(MessageSize);
+
+            // 헤더와 데이터 합치기
+            byte[] header = LengthBytes;
+            byte[] packet = new byte[header.Length + bytes.Length];
+            Array.Copy(header, packet, header.Length);
+            Array.Copy(bytes, 0, packet, header.Length, bytes.Length);
+            // 반드시 이게 나중에 들어와야 쓸모없는 값 참조를 안함
+            Data = packet;
+        }
+        public void GetData(ref byte[] data)
+        {
+            data = Data;
         }
     }
 }
---
+++ LoginClient/LoginClient/PacketIDNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginClient
+{
+    public enum LOGIN_CLIENT_PACKET_ID : uint
+    {
+        LOGIN_CLIENT_TRY_LOGIN = 0,
+    }
+    public enum LOGIN_SERVER_PACKET_ID : uint
+    {
+        LOGIN_SERVER_LOGIN_RESULT = 0
+    }
+}
